08cc8bb52e1c5fc470c95b3cb094185df264c333
Neto322 <42181276+Neto322@users.noreply.github.com>
2019-05-09 10:05:52 -0700

    Sliders de colores
    Perros de colores

--- Assets/Sliders.cs
+++ Assets/Sliders.cs
@@ -12,7 +12,12 @@ public class Sliders : MonoBehaviour
     Slider sldDesintegrate;
     [SerializeField]
     Slider sldRimPower;
-
+    [SerializeField]
+    Slider sldR;
+    [SerializeField]
+    Slider sldG;
+    [SerializeField]
+    Slider sldB;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +30,8 @@ public class Sliders : MonoBehaviour
         render.material.SetFloat("_ColorPower", sldColorPower.value);
         render.material.SetFloat("_Desintegrate", sldDesintegrate.value);
         render.material.SetFloat("_RimPower", sldRimPower.value);
+        render.material.SetFloat("_R", sldR.value);
+        render.material.SetFloat("_G", sldG.value);
+        render.material.SetFloat("_B", sldB.value);
     }
 }
