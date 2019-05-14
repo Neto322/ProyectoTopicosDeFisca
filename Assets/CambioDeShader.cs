5b3615a87b3bb4d46dcddb690c615b9319003ebb
Eduardo Antolín Jiménez <laloantolin@hotmail.com>
2019-05-14 10:03:54 -0700

    CanchaFutbol
    Cambiamos la escena por una canchita de futbol pa hacer un roket lig

---
+++ Assets/CambioDeShader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioDeShader : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    Shader Shader2;
+    [SerializeField]
+    Shader Shader1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+        Shader1 = Shader.Find("Shader Graphs/Shader");
+        Shader2 = Shader.Find("Shader Graphs/Shader2");
+        render.material.shader = Shader1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Shader Cambiado");
+            render.material.shader = Shader2;
+        }
+    }
+}
