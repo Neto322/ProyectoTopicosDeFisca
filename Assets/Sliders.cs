6dc2378be085d55fb710c54ea910059dbad63996
Neto322 <42181276+Neto322@users.noreply.github.com>
2019-05-22 22:54:51 -0700

    Jelp Mii

---
+++ Assets/Sliders.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sliders : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    Slider sldColorPower;
+    [SerializeField]
+    Slider sldDesintegrate;
+    [SerializeField]
+    Slider sldRimPower;
+    [SerializeField]
+    Slider sldR;
+    [SerializeField]
+    Slider sldG;
+    [SerializeField]
+    Slider sldB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        render.material.SetFloat("_ColorPower", sldColorPower.value);
+        render.material.SetFloat("_Desintegrate", sldDesintegrate.value);
+        render.material.SetFloat("_RimPower", sldRimPower.value);
+        render.material.SetFloat("_R", sldR.value);
+        render.material.SetFloat("_G", sldG.value);
+        render.material.SetFloat("_B", sldB.value);
+    }
+}
